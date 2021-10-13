b8f38ae1e2f160d3e78a82474743973419c1e067
dansal0807 <danielsaldanha0@gmail.com>
2021-10-13 11:56:41 -0300

    As páginas separadas são juntadas em um arquivo só.

--- edit-pdf.py
+++ edit-pdf.py
@@ -9,13 +9,29 @@ def extract_information(pdf_path):
 
 def split_pages(path, name_of_split):
     pdf = PdfFileReader(path)
-    for page in range((int(range_pages1)-1), int(range_pages2)):
+    for page in range((int(range_pages1)), int(range_pages2)):
         pdf_writer = PdfFileWriter()
         pdf_writer.addPage(pdf.getPage(page))
 
         output = f'{name_of_split}{page}.pdf'
         with open(output, 'wb') as output_pdf:
             pdf_writer.write(output_pdf)
+
+#paths = lista de pdfs
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
+
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # Add each page to the writer object
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+    # Write out the merged PDF
+    output = output + '.pdf'
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+    return output
 
 pdf_user = input(f"Diga-me o nome do PDF que você deseja abrir:\n")
 if '.pdf' not in pdf_user:
@@ -28,4 +44,10 @@ range_pages1 = input(f"\nDiga-me de que página você deseja começar o seu cort
 range_pages2 = input(f"\nDiga-me a página final que você deseja cortar seu PDF:\n")
 new_pdf_name = input(f"\nAgora diga o nome do seu novo PDF:\n")
 
+pages = []
+for i in range((int(range_pages1)), int(range_pages2)):
+    pdfs_final = f'{new_pdf_name}{i}' + '.pdf'
+    pages.append(pdfs_final)
+
 split_pages(pdf_user, new_pdf_name)
+merge_pdfs(pages, new_pdf_name)
